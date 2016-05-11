2df6bdc59da1be08523d099d19c7ba33b1dabe8a
Cameron Martin <cameronmartin123@gmail.com>
2016-05-11 20:28:32 +0100

    Changed eslintrc to satisfy codeclimate

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,12 @@ module.exports = {
       "flow-vars"
     ],
     "rules": {
-        "indent": ["error", 2, { VariableDeclarator: {var: 2, let: 2, const: 3} }],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single", { allowTemplateLiterals: true, avoidEscape: true }],
-        "semi": ["error", "always"],
-        "no-unused-vars": ["error", { vars: "all", args: "none" }],
+        "indent": [2, 2, { VariableDeclarator: {var: 2, let: 2, const: 3} }],
+        "linebreak-style": [2, "unix"],
+        "quotes": [2, "single", { allowTemplateLiterals: true, avoidEscape: true }],
+        "semi": [2, "always"],
+        "no-unused-vars": [2, { vars: "all", args: "none" }],
+
         "flow-vars/define-flow-type": 1,
         "flow-vars/use-flow-type": 1
     },
